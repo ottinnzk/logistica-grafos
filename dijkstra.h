a36db662ea383cd4082d92b6fad0476d003252bb
OttinNZK <ottoettinger8@gmail.com>
2026-05-23 14:36:20 -0300

    Testando .h

---
+++ dijkstra.h
@@ -0,0 +1,10 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+#include <vector>
+#include <utility>
+using namespace std;
+
+void dijkstra(vector<vector<pair<int, int> > >& adj, int vertices, int origem, int destino);
+
+#endif
